4f53eadd795997d209249182611ea8aecb074f76
Zoltan <zdebre@gmail.com>
2013-10-14 23:45:07 +0100

    raffler created

---
+++ ember/raffler/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = '90bae6ddd1d05cf6dccf74897a8b98f417ea69b8fc3ea7598096e3973138c3096d1f4252693cb532fa6c2dbea2130a24592b30a41d798e84b8563d1c4e464d1b'
