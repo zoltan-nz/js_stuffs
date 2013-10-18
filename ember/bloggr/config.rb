59dc9a00e892c0f92be93825f714856f82115f1c
Zoltan <zdebre@gmail.com>
2013-10-18 02:08:54 +0100

    trymes

--- ember/bloggr/config.rb
+++ ember/bloggr/config.rb
@@ -6,6 +6,9 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+activate :ember
+activate :emblem, emblem_dir: "templates", emblem_ext: "emblem", ignore: true
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
