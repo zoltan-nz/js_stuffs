1d232127bf1231a6e15a38884f168d5e54899d90
Zoltan <zdebre@gmail.com>
2013-11-01 19:37:44 +0000

    api for cafe vienna app

---
+++ jquerymobile/api/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jquerymobile::Application.config.secret_key_base = '3970887568cbf0fbfcefce344e8df3cb33ff3f3c566b098eb9534bb2c3877113e0f9c32af76bc5b6b4c515b82cf117ccbe27f592e379199d8df8e35b600781ea'
