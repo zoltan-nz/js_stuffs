76251c14f5b5bd6ad68cfc6681e9bed2dd507d92
Zoltan <zdebre@gmail.com>
2015-06-09 21:25:02 +1200

    Add file upload progress counter

--- nodejs/stream.js
+++ nodejs/stream.js
@@ -3,9 +3,22 @@ import http from 'http';
 
 http.createServer(function(request, response) {
     var newFile = fs.createWriteStream('readme_copy.md');
+    var fileBytes = request.headers['content-length'];
+    var uploadedBytes = 0;
+
+    request.on('readable', function() {
+        var chunk = null;
+
+        while(null !== (chunk = request.read())) {
+            uploadedBytes += chunk.length;
+            var progress = (uploadedBytes / fileBytes) * 100;
+            response.write("progress: " + parseInt(progress, 10) + "%\n");
+        }
+    });
+
     request.pipe(newFile);
 
     request.on('end', function() {
-        response.end('uploaded!');
+        response.end('uploaded!\n');
     });
 }).listen(8080);
