0d158e6cba54480f077527e2f017696ac2ec7e73
Zoltan <zdebre@gmail.com>
2015-06-12 20:20:10 +1200

    Add express

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,3 +1,12 @@
 import request from 'request';
 import url from 'url';
 
+var app = express();
+
+app.get('/hello/:nickname', function(req, response) {
+    "use strict";
+
+    var nickname = req.params.nickname;
+
+    response.write(`Hello ${nickname}`);
+})
